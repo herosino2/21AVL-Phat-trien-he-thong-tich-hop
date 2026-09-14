2843fd65bc729736dafd94190e19a3376130067e
herosino2 <minhhieutlm13504@gmail.com>
2026-09-14 22:22:03 +0700

    Remove SocketTCP folder

--- SocketTCP/README.md
+++
@@ -1,2 +0,0 @@
-# 21AVL-Phat-trien-he-thong-tich-hop
-Học kì 3 - môn Phát triển hệ thống tích hợp - IUH
--- SocketTCP/bai1/bai5.java
+++
@@ -1,3 +0,0 @@
-import java.util.Scanner;
-public class ExampleJava {
-     public static void main(String[] args) {
